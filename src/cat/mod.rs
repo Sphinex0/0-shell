c06d0194cca520dbf272e41c2c501ad75ba42896
root <root@DESKTOP-HDJVCVF>
2025-05-22 16:03:58 +0100

    added cat logic and copy logic note:still not fully tested

--- src/cat/mod.rs
+++ src/cat/mod.rs
@@ -1,3 +1,13 @@
-pub fn cat(tab: &[&str]) {
-
+pub fn cat(args: &[&str]) {
+    if args.is_empty() {
+        eprintln!("cat: missing file name");
+        return;
+    } else {
+        for filename in args {
+            match std::fs::read_to_string(filename) {
+                Ok(contents) => print!("{}", contents),
+                Err(err) => eprintln!("cat: {}: {}", filename, err),
+            }
+        }
+    }
 }
