06e75ad6033509a800462477429bf911cfefc775
Mustapha Boutoub <mustaphaboutoubdev@gmail.com>
2025-05-22 12:36:46 +0100

    start main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::io::Write;
+use std::io::stdin;
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        print!("$ ");
+        std::io::stdout().flush().unwrap();
+        let input = {
+            let mut buf = String::new();
+            stdin().read_line(&mut buf).unwrap();
+            buf
+        };
+        let input = input.split_whitespace().collect::<Vec<_>>();
+        let command = input[0];
+        let args = &input[1..];
+        let commands = [
+            "echo", "cd", "ls", "pwd", "cat", "cp", "rm", "mv", "mkdir", "exit",
+        ];
+        match commands.contains(&command) {
+            true => match command {
+                "echo" => {
+                    println!("edddd");
+                }
+                _ => {}
+            },
+            false => println!("err")
+        }
+    }
 }
