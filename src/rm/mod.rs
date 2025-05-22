7b2cf028cc081d72c066170b1309b02f99f90ab8
aelmir <adnane.elmir1@gmail.com>
2025-05-22 17:07:15 +0100

    save

--- src/rm/mod.rs
+++ src/rm/mod.rs
@@ -1,3 +1,19 @@
-pub fn rm(tab: &[&str]) {
-    
+use std::fs;
+use std::path::PathBuf;
+pub fn rm(args: &[&str], current_dir: &PathBuf) {
+    let path_copy = &mut current_dir.clone();
+        for arg in args {
+            if arg != &"-r" {
+                let mut tmp = path_copy.clone();
+                tmp.push(arg);
+                match tmp.read_dir(){
+                    Ok(_files)=>{
+                        fs::remove_dir_all(tmp).unwrap()
+                    },
+                    Err(_err)=>fs::remove_file(tmp).unwrap()
+                }
+            }
+        }
 }
+
+
