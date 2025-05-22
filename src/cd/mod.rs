2a83c2bb1f3e78aa4c1b0ad4ce4b39bb6acefa24
Mustapha Boutoub <mustaphaboutoubdev@gmail.com>
2025-05-22 15:21:11 +0100

    add cd

--- src/cd/mod.rs
+++ src/cd/mod.rs
@@ -1,18 +1,26 @@
 use std::path::PathBuf;
 
 pub fn cd(tab: &[&str], current_dir: &mut PathBuf) {
+    if tab.len() == 0 {
+        current_dir.clear();
+    }
     match tab[0] {
-        "../" => {
+        "../" | ".." => {
             current_dir.pop();
         }
-        path => {
+        "." => {}
+        mut path => {
+            path = match path.strip_prefix("./") {
+                Some(p) => p,
+                None => path
+            } ;
             current_dir.push(path);
             match current_dir.read_dir() {
-                Ok(_) => {},
+                Ok(_) => {}
                 Err(_) => {
                     current_dir.pop();
                     println!("No such directory")
-                },
+                }
             }
         }
     };
