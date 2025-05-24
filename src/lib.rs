2b3ebc564afa5cae6e804c8c28be3722b5e68530
Mustapha Boutoub <mustaphaboutoubdev@gmail.com>
2025-05-24 17:38:49 +0100

    after this is good

--- src/lib.rs
+++ src/lib.rs
@@ -20,46 +20,69 @@ pub use pwd::*;
 pub use rm::*;
 
 pub trait CostumSplit {
-    fn costum_split(&self) -> (Vec<String>, bool, Vec<Vec<String>>);
+    fn costum_split(&self) -> (Vec<String>, bool);
 }
 impl CostumSplit for String {
-    fn costum_split(&self) -> (Vec<String>, bool, Vec<Vec<String>>) {
+    fn costum_split(&self) -> (Vec<String>, bool) {
         let mut result: Vec<String> = Vec::new();
-        let mut big_table: Vec<Vec<String>> = Vec::new();
         let mut arg = String::new();
         let mut open_quote = false;
-        for ch in self.chars() {
-            if ch == '"' {
-                open_quote = !open_quote;
-                continue;
-            }
-            if ch == ';' && !open_quote {
-                if !arg.is_empty() {
-                    result.push(arg);
-                    arg = String::new();
+
+        // for (i, ch) in self.char_indices() {
+        //     if ch == '\\' && self[i + 1..i + 2].chars().next().unwrap_or(' ') == '\\' {}
+
+        //     if (ch == '"' && i == 0)
+        //         || (ch == '"' && self[i - 1..i].chars().next().unwrap_or(' ') != '\\')
+        //     {
+        //         open_quote = !open_quote;
+        //         continue;
+        //     }
+        //     if ch.is_whitespace() && !open_quote {
+        //         if !arg.is_empty() {
+        //             result.push(arg);
+        //             arg = String::new();
+        //         }
+        //     } else {
+        //         arg.push(ch)
+        //     }
+        // }
+
+        let mut chars = self.chars().peekable();
+        while let Some(ch) = chars.next() {
+            match ch {
+                '\\' => match chars.peek() {
+                    Some('\\') => {
+                        arg.push('\\');
+                        chars.next();
+                    }
+                    Some('"') => {
+                        arg.push('"');
+                        chars.next();
+                    }
+                    _ => {}
+                },
+                _ => {
+                    if ch == '"' {
+                        open_quote = !open_quote;
+                        continue;
+                    }
+                    if ch.is_whitespace() && !open_quote {
+                        if !arg.is_empty() {
+                            result.push(arg);
+                            arg = String::new();
+                        }
+                    } else {
+                        arg.push(ch)
+                    }
                 }
-                big_table.push(result.clone());
-                result = vec![];
-                continue;
-            }
-            if ch.is_whitespace() && !open_quote {
-                if !arg.is_empty() {
-                    result.push(arg);
-                    arg = String::new();
-                }
-            } else {
-                arg.push(ch)
             }
         }
+
         if !arg.is_empty() {
             result.push(arg);
         }
 
-        if !result.is_empty() {
-            big_table.push(result.clone());
-        }
-
-        (result, open_quote, big_table)
+        (result, open_quote)
     }
 }
 
