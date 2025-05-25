999513513de7ec49cfb8c85b5348b2eb4e5649e2
Mustapha Boutoub <mustaphaboutoubdev@gmail.com>
2025-05-25 10:35:57 +0100

    after this is good

--- src/lib.rs
+++ src/lib.rs
@@ -28,6 +28,61 @@ impl CostumSplit for String {
         let mut arg = String::new();
         let mut open_quote = false;
 
+
+        // let mut chars = self.chars().peekable();
+        // while let Some(ch) = chars.next() {
+        //     println!("ch: {ch}");
+        //     match ch {
+        //         '\\' => match chars.next() {
+        //             Some(ch2) => match (ch2, open_quote) {
+        //                 ('n', true) => arg.push('\n'),
+        //                 ('r', true) => arg.push('\r'),
+        //                 ('t', true) => arg.push('\t'),
+        //                 ('a', true) => arg.push(7 as char),
+        //                 ('b', true) => arg.push(8 as char),
+        //                 ('\\', false) => match chars.next() {
+        //                     Some(ch3) => match ch3 {
+        //                         'n' => arg.push('\n'),
+        //                         'r' => arg.push('\r'),
+        //                         't' => arg.push('\t'),
+        //                         'a' => arg.push(7 as char),
+        //                         'b' => arg.push(8 as char),
+        //                         _ => {
+        //                             arg.push_str(&format!("\\{ch3}"));
+        //                         }
+        //                     },
+        //                     None => arg.push('\\'),
+        //                 },
+        //                 (a, true) => {
+        //                     // println!("dddddd => {:?}", a);
+        //                     arg.push(a);
+        //                 }
+        //                 (b, false) => {
+        //                     if b.is_whitespace() {
+        //                         result.push(arg);
+        //                         arg = String::new();
+        //                     } else {
+        //                         arg.push(b);
+        //                     }
+        //                 }
+        //             },
+        //             None => {}
+        //         },
+        //         '"' => {
+        //             open_quote = !open_quote;
+        //         }
+        //         _ => {
+        //             if ch.is_whitespace() && !open_quote {
+        //                 result.push(arg);
+        //                 arg = String::new();
+        //             } else {
+        //                 arg.push(ch);
+        //             }
+        //         }
+        //     }
+        // }
+
+        // originale code 
         for (i, ch) in self.char_indices() {
             if (ch == '"' && i == 0)
                 || (ch == '"' && self[i - 1..i].chars().next().unwrap_or(' ') != '\\')
@@ -45,40 +100,13 @@ impl CostumSplit for String {
             }
         }
 
-        // let mut chars = self.chars().peekable();
-        // while let Some(ch) = chars.next() {
-        //     match ch {
-        //         '\\' => match chars.peek() {
-        //             Some('\\') => {
-        //                 arg.push('\\');
-        //                 chars.next();
-        //             }
-        //             Some('"') => {
-        //                 arg.push('"');
-        //                 chars.next();
-        //             }
-        //             _ => {}
-        //         },
-        //         _ => {
-        //             if ch == '"' {
-        //                 open_quote = !open_quote;
-        //                 continue;
-        //             }
-        //             if ch.is_whitespace() && !open_quote {
-        //                 if !arg.is_empty() {
-        //                     result.push(arg);
-        //                     arg = String::new();
-        //                 }
-        //             } else {
-        //                 arg.push(ch)
-        //             }
-        //         }
-        //     }
-        // }
+
 
         if !arg.is_empty() {
             result.push(arg);
         }
+
+        // println!("{:?}", result);
 
         (result, open_quote)
     }
@@ -87,7 +115,3 @@ impl CostumSplit for String {
 pub fn print_error(message: &str) {
     eprintln!("\x1b[31m {}\x1b[0m", message)
 }
-
-
-
-
--- src/main.rs
+++ src/main.rs
@@ -37,6 +37,8 @@ fn main() {
             continue;
         }
 
+        println!("{}",open_quote);
+
         if open_quote {
             loop {
                 print!("\x1b[33m> \x1b[0m");
