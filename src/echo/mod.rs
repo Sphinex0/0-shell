7171c2f66698556b857dcd0df198ac99754851a1
aelmir <adnane.elmir1@gmail.com>
2025-05-29 19:17:36 +0100

    handling backtick

--- src/echo/mod.rs
+++ src/echo/mod.rs
@@ -1,65 +1,166 @@
-pub fn echo(args: &[String], _entry: &String)->String {
-    // println!("{}", args.join(""));
-    args.join("")
-    // parse_entry(entry);
+pub fn echo(args: &[String], _entry: &String)-> String {
+    // println!("echo args:{args:?}");
+    let entry = args.join("");
+    let (res, newline) = parse_entry(&entry);
+    if newline {
+        format!("{}", res)
+    } else {
+        format!("{}", res)
+    }
 }
 
-fn parse_entry(entry: &String) -> String {
-    let le: usize = entry.split_whitespace().nth(0).unwrap().len();
-    let entry = entry[le + 1..].to_string();
-    let mut open_quote = false;
-    let mut result: String = String::new();
+fn parse_entry(entry: &str) -> (String, bool) {
+    let mut result = String::new();
     let mut chars = entry.chars().peekable();
+
     while let Some(ch) = chars.next() {
         match ch {
-            '\\' => match chars.next() {
-                Some(ch2) => match (ch2, open_quote) {
-                    ('n', true) => result.push('\n'),
-                    ('r', true) => result.push('\r'),
-                    ('t', true) => result.push('\t'),
-                    ('a', true) => result.push(7 as char),
-                    ('b', true) => result.push(8 as char),
-                    ('\\', false) => match chars.next() {
-                        Some(ch3) => match ch3 {
-                            'n' => result.push('\n'),
-                            'r' => result.push('\r'),
-                            't' => result.push('\t'),
-                            'a' => result.push(7 as char),
-                            'b' => result.push(8 as char),
-                            _ => {
-                                result.push_str(&format!("\\{ch3}"));
+            '\\' => {
+                if let Some(next_ch) = chars.next() {
+                    match next_ch {
+                        'n' => result.push('\n'),
+                        'r' => result.push('\r'),
+                        't' => result.push('\t'),
+                        'a' => result.push('\x07'),
+                        'b' => result.push('\x08'),
+                        'c' => return (result, false),
+                        'e' => result.push('\x1B'),
+                        'f' => result.push('\x0C'),
+                        'v' => result.push('\x0B'),
+                        '\\' => result.push('\\'),
+                        '0' => match chars.peek() {
+                            //1
+                            Some(&ch) => {
+                                let mut octal = String::new();
+                                if ch.is_digit(8) {
+                                    octal.push(ch);
+                                    chars.next();
+                                    //2
+                                    match chars.peek() {
+                                        Some(&ch2) => {
+                                            if ch2.is_digit(8) {
+                                                octal.push(ch2);
+                                                chars.next();
+                                                //3
+                                                match chars.peek() {
+                                                    Some(&ch3) => {
+                                                        if ch3.is_digit(8) {
+                                                            octal.push(ch3);
+                                                            chars.next();
+                                                        }
+                                                    }
+                                                    None => {}
+                                                };
+                                            }
+                                        }
+                                        None => {}
+                                    };
+                                }
+                                match u8::from_str_radix(&octal, 8) {
+                                    Ok(val) => result.push(val as char),
+                                    _ => {}
+                                }
                             }
+                            None => {}
                         },
-                        None => result.push('\\'),
-                    },
-                    a => {
-                        result.push(a.0);
+                        _ => {
+                            result.push(ch);
+                            result.push(next_ch);
+                        }
                     }
-                },
-                None => {}
-            },
-            '"' => {
-                open_quote = !open_quote;
+                } else {
+                    result.push('\\');
+                }
             }
             _ => result.push(ch),
         }
     }
 
-    let mut chs = result.chars().peekable();
-    let mut res = String::new();
-    while let Some(ch) = chs.next() {
-        match ch {
-            '\\' => match chs.peek() {
-                Some('\\') => {
-                    res.push('\\');
-                    chs.next();
-                }
-                _ => {}
-            },
-            _ => res.push(ch),
-        }
-    }
-    // print!("{:#?}", res);
-    // print!("{res}");
-    res
+    (result, true)
 }
+
+//   \b      A backspace character is output.
+
+//             \c      Subsequent output is suppressed.  This is normally used at the end of the last argument to suppress the trailing newline that echo would otherwise output.
+
+//             \e      Outputs an escape character (ESC).
+
+//             \f      Output a form feed.
+
+//             \n      Output a newline character.
+
+//             \r      Output a carriage return.
+
+//             \t      Output a (horizontal) tab character.
+
+//             \v      Output a vertical tab.
+
+//             \0digits
+//                     Output the character whose value is given by zero to three octal digits.  If there are zero digits, a nul character is output.
+
+//             \\      Output a backslash.
+
+//             All other backslash sequences elicit undefined behaviour.
+
+// fn parse_entry(entry: &String) {
+//     let le: usize = entry.split_whitespace().nth(0).unwrap().len();
+//     let entry: String = entry[le + 1..].to_string();
+//     // let mut open_quote = false;
+//     let mut result: String = String::new();
+//     let mut chars = entry.chars().peekable();
+//     while let Some(ch) = chars.next() {
+//         match ch {
+//             '\\' => match chars.next() {
+//                 Some(ch2) => match (ch2) {
+//                     ('n', true) => result.push('\n'),
+//                     ('r', true) => result.push('\r'),
+//                     ('t', true) => result.push('\t'),
+//                     ('a', true) => result.push(7 as char),
+//                     ('b', true) => result.push(8 as char),
+//                     ('\\', false) => {
+//                         /*
+//                             match chars.next() {
+//                             Some(ch3) => match ch3 {
+//                                 'n' => result.push('\n'),
+//                                 'r' => result.push('\r'),
+//                                 't' => result.push('\t'),
+//                                 'a' => result.push(7 as char),
+//                                 'b' => result.push(8 as char),
+//                                 _ => {
+//                                     result.push_str(&format!("\\{ch3}"));
+//                                 }
+//                             },
+//                             None => result.push('\\'),
+//                         } */
+//                        result.push('\\');
+//                     }
+//                     a => {
+//                         result.push(a.0);
+//                     }
+//                 },
+//                 None => {}
+//             },
+//             '"' => {
+//                 open_quote = !open_quote;
+//             }
+//             _ => result.push(ch),
+//         }
+//     }
+
+//     // let mut chs = result.chars().peekable();
+//     // let mut res = String::new();
+//     // while let Some(ch) = chs.next() {
+//     //     match ch {
+//     //         '\\' => match chs.peek() {
+//     //             Some('\\') => {
+//     //                 res.push('\\');
+//     //                 chs.next();
+//     //             }
+//     //             _ => {}
+//     //         },
+//     //         _ => res.push(ch),
+//     //     }
+//     // }
+//     // print!("{:#?}", res);
+//     print!("{res}");
+// }
--- src/lib.rs
+++ src/lib.rs
@@ -33,11 +33,11 @@ pub struct Command {
 
 impl Command {
     pub fn add_string(&mut self, word: &String) {
-        if word.len() == 0 {
+        if word.is_empty() {
             return;
         }
         if self.name.is_empty() {
-            self.name = word.clone()
+            self.name = word.clone();
         } else {
             self.args.push(CommandPart::String(word.clone()));
         }
@@ -45,106 +45,137 @@ impl Command {
 }
 
 pub trait CostumSplit {
-    fn costum_split(&self) -> (Command, bool);
+    fn custom_split(&self) -> (Command, bool);
 }
 
 impl CostumSplit for String {
-    fn costum_split(&self) -> (Command, bool) {
+    fn custom_split(&self) -> (Command, bool) {
+        // println!("self => :{self:?}");
         let mut command = Command {
             name: String::new(),
             args: Vec::new(),
         };
         let mut word = String::new();
-        let mut main_command = false;
+        let mut state = State::Normal;
+        let mut open_backslash = false;
+        let mut backtick_str = String::new();
 
-        // commands.push(Vec::new());
+        #[derive(Debug, PartialEq)]
+        enum State {
+            Normal,
+            DoubleQuote,
+            SingleQuote,
+            Backtick,
+        }
 
-        let mut backtick_result: Vec<String> = Vec::new();
-        let mut backtick_arg: String = String::new();
-
-        let mut open_double_quote = false;
-        let mut open_single_quote = false;
-        let mut open_backslash_quote = false;
-        let mut open_backtick_quote = false;
-
-        let special = ['"', '\'', '\\', '`'];
-
-        let mut chars = self.chars().peekable();
-        while let Some(ch) = chars.next() {
-            match ch {
-                '"' => open_double_quote = !open_double_quote,
-                '\'' => open_single_quote = !open_single_quote,
-                '`' => {
-                    open_backtick_quote = !open_backtick_quote;
-
-                    if open_backtick_quote {
-                        command.args.push(CommandPart::Substitution(Command {
-                            name: String::new(),
-                            args: Vec::new(),
-                        }));
-                    } else {
-                        let last_index = command.args.len() - 1;
-
-                        if let CommandPart::Substitution(sub_command) =
-                            &mut command.args[last_index]
-                        {
-                            sub_command.add_string(&word);
-                        };
-                        word.clear();
-                    }
+        let chs = self.split("\n").collect::<Vec<_>>();
+        for (i, line) in chs.iter().enumerate() {
+            if matches!(state, State::DoubleQuote | State::SingleQuote) {
+                // command.add_string(&"\n".to_string());
+                word.push('\n');
+            }
+            if line.is_empty() {
+                if open_backslash && i != chs.len() - 1 {
+                    open_backslash = false;
                 }
-                '\\' => {
-                    // if !open_backslash_quote {
-                    //     open_backslash_quote = true;
-                    //     match chars.peek() {
-                    //         Some(ch2) => {
-                    //             open_backslash_quote = false;
-                    //             arg.push(*ch2);
-                    //             chars.next();
-                    //         }
-                    //         _ => {}
-                    //     }
-                    // }
-                }
-
-                _ => {
-                    if ch.is_whitespace()
-                        && !(open_double_quote || open_single_quote || open_backslash_quote)
-                    {
-                        if open_backtick_quote {
-                            let last_index = command.args.len() - 1;
-                            if let CommandPart::Substitution(sub_command) =
-                                &mut command.args[last_index]
-                            {
-                                sub_command.add_string(&word);
-                            };
-                        } else {
+            }
+            let mut chars = line.chars().peekable();
+            while let Some(ch) = chars.next() {
+                match state {
+                    State::Normal => {
+                        if ch.is_whitespace() && !open_backslash {
                             command.add_string(&word);
 
                             let le = command.args.len();
                             if le > 0
                                 && command.args[le - 1] != CommandPart::String(" ".to_string())
                             {
-                                command.args.push(CommandPart::String(" ".to_string()));
+                                command.add_string(&" ".to_string());
+                            }
+
+                            word.clear();
+                        } else if ch == '"' && !open_backslash {
+                            state = State::DoubleQuote;
+                        } else if ch == '\'' && !open_backslash {
+                            state = State::SingleQuote;
+                        } else if ch == '`' && !open_backslash {
+                            command.add_string(&word);
+                            word.clear();
+
+                            state = State::Backtick;
+                            backtick_str.clear();
+                        } else if ch == '\\' {
+                            open_backslash = true;
+                        } else {
+                            if open_backslash {
+                                word.push(ch);
+                                open_backslash = false;
+                            } else {
+                                word.push(ch);
                             }
                         }
-
-                        word.clear();
-                    } else {
-                        word.push(ch);
+                    }
+                    State::DoubleQuote => {
+                        if ch == '"' && !open_backslash {
+                            state = State::Normal;
+                        } else if ch == '\\' {
+                            // println!("ff");
+                            open_backslash = true;
+                            match chars.peek() {
+                                Some(&ch2) => {
+                                    open_backslash = false;
+                                    word.push(ch2);
+                                    chars.next();
+                                }
+                                None => {}
+                            }
+                        } else {
+                            if open_backslash {
+                                if ['"', '\\', '`', '$'].contains(&ch) {
+                                    word.push(ch);
+                                } else {
+                                    word.push('\\');
+                                    word.push(ch);
+                                }
+                                open_backslash = false;
+                            } else {
+                                word.push(ch);
+                            }
+                        }
+                    }
+                    State::SingleQuote => {
+                        if ch == '\'' {
+                            state = State::Normal;
+                        } else {
+                            word.push(ch);
+                        }
+                    }
+                    State::Backtick => {
+                        if ch == '`' {
+                            // let (nested_command, _) = backtick_str.custom_split();
+                            let (nested_command, _) = backtick_str.custom_split();
+                            command.args.push(CommandPart::Substitution(nested_command));
+                            println!("gg => :{command:?}");
+                            state = State::Normal;
+                        } else {
+                            backtick_str.push(ch);
+                        }
                     }
                 }
             }
+
+            // if !word.is_empty() {
+            //     command.add_string(&word);
+            // }
         }
 
-        /* flush anything left */
-        command.add_string(&word);
+        if !word.is_empty() {
+            command.add_string(&word);
+        }
 
-        // println!("{:?}", result);
-
-        let open =
-            open_double_quote || open_single_quote || open_backslash_quote || open_backtick_quote;
-        // println!("command : {command:#?}");
+        let open = matches!(state, State::DoubleQuote | State::SingleQuote)
+            || open_backslash
+            || state == State::Backtick;
         (command, open)
     }
 }
