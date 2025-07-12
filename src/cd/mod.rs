485e0aae9eff6cc6032e996e5216470f5505188d
killux = Mustapha Boutoub <mustaphaboutoubdev@gmail.com>
2025-07-12 11:04:10 +0100

    remeve space in splite

--- src/cd/mod.rs
+++ src/cd/mod.rs
@@ -7,14 +7,7 @@ use crate::print_error;
 
 pub fn cd(tab: &[String], history: &mut PathBuf, current_di: &mut PathBuf, home: &PathBuf) {
     let mut path = tab.get(0).unwrap_or(&home.display().to_string()).clone();
-    // if tab.len() == 0 {
-    //     path = ;
-    // } else {
-    //     path = tab[0].to_string();
-    // }
     let mut change = true;
-
-    // let last_dir = current_di.clone();
     match path.as_str() {
         "-" => {
             if let Err(err) = set_current_dir(history.clone()) {
