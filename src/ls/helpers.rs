c667013a96553be5bdba70bb53201d6f932d016f
houtayb <houtayb@gmail.com>
2025-07-13 11:34:58 +0100

    costumise code and minimize

---
+++ src/ls/helpers.rs
@@ -0,0 +1,111 @@
+use chrono::Datelike;
+use chrono::{DateTime, Local};
+use std::fs;
+use std::fs::Metadata;
+use std::fs::Permissions;
+use std::os::unix::fs::MetadataExt;
+use std::os::unix::fs::PermissionsExt;
+use std::path::Path;
+use std::time::SystemTime;
+use users::*;
+
+// helpers
+pub fn is_executable(path: &Path) -> bool {
+    if let Ok(metadata) = fs::metadata(path) {
+        let mode = metadata.permissions().mode();
+        mode & 0o111 != 0
+    } else {
+        false
+    }
+}
+
+pub fn format_permissions(permissions: &Permissions) -> String {
+    let mode = permissions.mode();
+    let owner = mode >> 6;
+    let group = mode >> 3;
+    let others = mode;
+
+    let mut perm_str = String::with_capacity(9);
+    perm_str.push(if owner & 0o4 != 0 { 'r' } else { '-' });
+    perm_str.push(if owner & 0o2 != 0 { 'w' } else { '-' });
+    perm_str.push(if owner & 0o1 != 0 { 'x' } else { '-' });
+
+    perm_str.push(if group & 0o4 != 0 { 'r' } else { '-' });
+    perm_str.push(if group & 0o2 != 0 { 'w' } else { '-' });
+    perm_str.push(if group & 0o1 != 0 { 'x' } else { '-' });
+
+    perm_str.push(if others & 0o4 != 0 { 'r' } else { '-' });
+    perm_str.push(if others & 0o2 != 0 { 'w' } else { '-' });
+    perm_str.push(if others & 0o1 != 0 { 'x' } else { '-' });
+
+    perm_str
+}
+
+pub fn get_usr(metadata: &Metadata) -> Option<User> {
+    let uid = metadata.uid();
+    let user = get_user_by_uid(uid);
+    user
+}
+
+pub fn get_grp(metadata: &Metadata) -> Group {
+    let gid = metadata.gid();
+
+    match get_group_by_gid(gid) {
+        Some(group) => group,
+        None => get_group_by_gid(0).unwrap_or(Group::new(gid, "root")),
+    }
+}
+
+pub fn get_symlink_target_name<P: AsRef<Path>>(
+    symlink_path: P,
+) -> Result<(Result<Metadata, std::io::Error>, String), String> {
+    // Read the target path of the symlink
+    let meta: Result<Metadata, std::io::Error> = fs::metadata(&symlink_path);
+
+    let target_path = match fs::read_link(&symlink_path) {
+        Ok(path) => path,
+        Err(err) => {
+            return Err(format!(
+                "Failed to read symlink '{}': {}",
+                symlink_path.as_ref().display(),
+                err
+            ));
+        }
+    };
+
+    // Get the file name from the target path
+    let target_name = match target_path.file_name() {
+        Some(name) => name,
+        None => {
+            return Err(format!(
+                "Symlink '{}' points to an invalid path: {}",
+                symlink_path.as_ref().display(),
+                target_path.display()
+            ));
+        }
+    };
+
+    // Convert OsStr to String
+    let name = target_name
+        .to_str()
+        .map(String::from)
+        .unwrap_or("".to_string());
+
+    Ok((meta, name))
+}
+
+pub fn get_time(metadata: &Metadata) -> String {
+    let name = iana_time_zone::get_timezone().unwrap();
+    let tz = name.parse::<chrono_tz::Tz>().unwrap();
+    let last_mod_time = metadata.modified().unwrap_or(SystemTime::UNIX_EPOCH);
+    let datetime: DateTime<Local> = last_mod_time.into();
+    let datetime = datetime.with_timezone(&tz);
+
+    let mut formatted_time = datetime.format("%b %e %H:%M").to_string();
+    let current_year = Local::now().year();
+    let its_year = datetime.year();
+    if current_year != its_year {
+        formatted_time = datetime.format("%b %e  %Y").to_string();
+    };
+    formatted_time
+}
--- src/ls/mod.rs
+++ src/ls/mod.rs
@@ -1,17 +1,14 @@
-use chrono::Datelike;
-use chrono::{DateTime, Local};
+pub use helpers::*;
+use libc::major;
 use std::fs;
 use std::fs::DirEntry;
 use std::fs::Metadata;
-use std::fs::Permissions;
 use std::io::ErrorKind;
 use std::os::unix::fs::FileTypeExt;
 use std::os::unix::fs::MetadataExt;
-use std::os::unix::fs::PermissionsExt;
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 use term_size::dimensions;
-use users::*;
-use libc::{major, minor};
+pub mod helpers;
 
 #[derive(Debug)]
 struct Fileinfo {
@@ -104,8 +101,6 @@ impl Ls {
             self.files.push(self.get("."));
             self.files.push(self.get(".."));
         }
-        let name = iana_time_zone::get_timezone().unwrap();
-        let tz = name.parse::<chrono_tz::Tz>().unwrap();
 
         for entry in entries {
             let metadata = entry.metadata().unwrap();
@@ -113,14 +108,11 @@ impl Ls {
             let mut file = Fileinfo::new(metadata.clone());
 
             // Get user and group info
-            let user = get_usr(&metadata).unwrap();
-            let grp = get_grp(&metadata);
+            let user = helpers::get_usr(&metadata).unwrap();
+            let grp = helpers::get_grp(&metadata);
             file.user = user.name().to_str().unwrap().to_string();
             file.group = grp.name().to_str().unwrap().to_string();
-            let last_mod_time = metadata.modified().unwrap();
-            let datetime: DateTime<Local> = last_mod_time.into();
-            let datetime = datetime.with_timezone(&tz);
-            let formatted_time = datetime.format("%b %e %H:%M").to_string();
+            let formatted_time = get_time(&file.metadata);
 
             max_user = max_user.max(file.user.len());
             max_group = max_group.max(file.group.len());
@@ -231,13 +223,13 @@ impl Ls {
                 } else if file_type.is_symlink() {
                     color = "\x1b[1;36m";
                     if let Some(en) = &file.entry {
-                        if let Ok((meta_data, mut name)) = get_symlink_target_name(&en) {
+                        if let Ok((meta_data, mut name)) = helpers::get_symlink_target_name(&en) {
                             match meta_data {
                                 Ok(meta) => {
                                     let mut color2 = "\x1b[0m";
                                     if meta.is_dir() {
                                         color2 = "\x1b[1;34m";
-                                    } else if meta.is_file() && is_executable(&en) {
+                                    } else if meta.is_file() && helpers::is_executable(&en) {
                                         color2 = "\x1b[1;32m";
                                     }
 
@@ -245,7 +237,7 @@ impl Ls {
                                         if meta.is_dir() {
                                             color2 = "\x1b[1;34m";
                                             name.push('/');
-                                        } else if meta.is_file() && is_executable(&en) {
+                                        } else if meta.is_file() && helpers::is_executable(&en) {
                                             name.push('*');
                                         }
                                     }
@@ -276,27 +268,17 @@ impl Ls {
                     '?'
                 };
 
-                let last_mod_time = file.metadata.modified().unwrap();
-                let datetime: DateTime<Local> = last_mod_time.into();
-                let datetime = datetime.with_timezone(&tz);
-
-                let mut formatted_time = datetime.format("%b %e %H:%M").to_string();
-                let current_year = Local::now().year();
-                let its_year = datetime.year();
-                if current_year != its_year {
-                    formatted_time = datetime.format("%b %e  %Y").to_string();
-                }
-
-                let perms = format_permissions(&permissions);
+                let formatted_time = get_time(&file.metadata);
+                let perms = helpers::format_permissions(&permissions);
                 let hardlink = file.metadata.nlink();
                 let file_size = file.metadata.len();
                 let size_field = if file_type.is_char_device() || file_type.is_block_device() {
                     let rdev = file.metadata.rdev();
-                    let major_num = major(rdev) ; 
-                   // let minor_num = minor(rdev) ; 
+                    let major_num = major(rdev);
+                    // let minor_num = minor(rdev) ;
                     format!("{ }, ", major_num)
                 } else {
-                        file_size.to_string()
+                    file_size.to_string()
                 };
 
                 res.push(format!(
@@ -411,7 +393,10 @@ pub fn ls(tab: &[String], current_dir: &PathBuf) -> String {
                     ),
                     ErrorKind::NotADirectory => {
                         let temp_dir = dir.clone();
-                        let file_name = temp_dir.file_name().unwrap().to_str().unwrap();
+                        let file_name: &str = temp_dir
+                            .file_name()
+                            .and_then(|os_str| os_str.to_str())
+                            .unwrap_or("unknown");
                         dir.pop();
                         match fs::read_dir(&dir) {
                             Ok(entries) => {
@@ -440,86 +425,4 @@ pub fn ls(tab: &[String], current_dir: &PathBuf) -> String {
         }
     }
     output
-}
-
-// helpers
-fn is_executable(path: &Path) -> bool {
-    if let Ok(metadata) = fs::metadata(path) {
-        let mode = metadata.permissions().mode();
-        mode & 0o111 != 0
-    } else {
-        false
-    }
-}
-
-fn format_permissions(permissions: &Permissions) -> String {
-    let mode = permissions.mode();
-    let owner = mode >> 6;
-    let group = mode >> 3;
-    let others = mode;
-
-    let mut perm_str = String::with_capacity(9);
-    perm_str.push(if owner & 0o4 != 0 { 'r' } else { '-' });
-    perm_str.push(if owner & 0o2 != 0 { 'w' } else { '-' });
-    perm_str.push(if owner & 0o1 != 0 { 'x' } else { '-' });
-
-    perm_str.push(if group & 0o4 != 0 { 'r' } else { '-' });
-    perm_str.push(if group & 0o2 != 0 { 'w' } else { '-' });
-    perm_str.push(if group & 0o1 != 0 { 'x' } else { '-' });
-
-    perm_str.push(if others & 0o4 != 0 { 'r' } else { '-' });
-    perm_str.push(if others & 0o2 != 0 { 'w' } else { '-' });
-    perm_str.push(if others & 0o1 != 0 { 'x' } else { '-' });
-
-    perm_str
-}
-
-fn get_usr(metadata: &Metadata) -> Option<User> {
-    let uid = metadata.uid();
-    let user = get_user_by_uid(uid);
-    user
-}
-
-fn get_grp(metadata: &Metadata) -> Group {
-    let gid = metadata.gid();
-
-    match get_group_by_gid(gid) {
-        Some(group) => group,
-        None => get_group_by_gid(0).unwrap_or(Group::new(gid, "root")),
-    }
-}
-
-fn get_symlink_target_name<P: AsRef<Path>>(
-    symlink_path: P,
-) -> Result<(Result<Metadata, std::io::Error>, String), String> {
-    // Read the target path of the symlink
-    let meta: Result<Metadata, std::io::Error> = fs::metadata(&symlink_path);
-
-    let target_path = match fs::read_link(&symlink_path) {
-        Ok(path) => path,
-        Err(err) => {
-            return Err(format!(
-                "Failed to read symlink '{}': {}",
-                symlink_path.as_ref().display(),
-                err
-            ));
-        }
-    };
-
-    // Get the file name from the target path
-    let target_name = match target_path.file_name() {
-        Some(name) => name,
-        None => {
-            return Err(format!(
-                "Symlink '{}' points to an invalid path: {}",
-                symlink_path.as_ref().display(),
-                target_path.display()
-            ));
-        }
-    };
-
-    // Convert OsStr to String
-    let name = target_name.to_str().map(String::from).unwrap();
-
-    Ok((meta, name))
 }
